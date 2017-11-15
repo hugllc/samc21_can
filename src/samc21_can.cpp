269f32b8317b0b212a8673a21579aa5c90b7d61c
Scott Price <prices@hugllc.com>
2017-11-15 16:08:17 -0600

    I got the first data sent with this checkin.  It is still really crude and needs to be cleaned up a lot, but it is sending data.

--- src/samc21_can.cpp
+++ src/samc21_can.cpp
@@ -7,6 +7,11 @@
  */
 
 #include "samc21_can.h"
+#include "Arduino.h"
+
+MCP_CAN *use_object;
+
+
 /**
 * @brief Checks for the presence of a connector id
 *
@@ -15,11 +20,98 @@
 * @return void
 */
 MCP_CAN::MCP_CAN(uint8_t _CS)
+: rx_ded_buffer_data(false)
 {
+    use_object = this;
 };
+
 uint8_t MCP_CAN::begin(uint8_t idmodeset, uint8_t speedset, uint8_t clockset)
 {
-    return 0;
+    uint8_t ret;
+    const struct mcan_config mcan_cfg = {
+
+        id : ID_CAN0,
+        regs : CAN0,
+        msg_ram : mcan_msg_ram,
+
+        array_size_filt_std : RAM_ARRAY_SIZE_FILT_STD,
+        array_size_filt_ext : RAM_ARRAY_SIZE_FILT_EXT,
+        fifo_size_rx0 : RAM_FIFO_SIZE_RX0,
+        fifo_size_rx1 : 0,
+        array_size_rx : RAM_ARRAY_SIZE_RX,
+        fifo_size_tx_evt : 0,
+        array_size_tx : RAM_ARRAY_SIZE_TX,
+        fifo_size_tx : RAM_FIFO_SIZE_TX,
+
+        buf_size_rx_fifo0 : MAX_CHAR_IN_MESSAGE,
+        buf_size_rx_fifo1 : 0,
+        buf_size_rx : MAX_CHAR_IN_MESSAGE,
+        buf_size_tx : MAX_CHAR_IN_MESSAGE,
+
+        /*
+        using values from AT6493 (SAMC21 app note); the plus values are to add on what the MCAN driver subtracts back off
+        */
+        bit_rate : 125000,
+        quanta_before_sp : 10 + 2,
+        quanta_after_sp : 3 + 1,
+
+        /*
+        AT6493 (SAMC21 app note) 'fast' values were unhelpfully the same as normal speed; these are for double (1MBit)
+                the maximum peripheral clock of 48MHz on the SAMC21 does restrict us from very high rates
+        */
+        bit_rate_fd : 125000,
+        quanta_before_sp_fd : 10 + 2,
+        quanta_after_sp_fd : 3 + 1,
+
+        quanta_sync_jump : 3 + 1,
+        quanta_sync_jump_fd : 3 + 1,
+    };
+    
+    
+    switch (mcan_cfg.id)
+    {
+    case ID_CAN0:
+        PORT->Group[0].DIRSET.reg = PORT_PA24;
+        PORT->Group[0].DIRCLR.reg = PORT_PA25;
+        PORT->Group[0].PINCFG[24].reg = PORT_PINCFG_INEN | PORT_PINCFG_PMUXEN;
+        PORT->Group[0].PINCFG[25].reg = PORT_PINCFG_INEN | PORT_PINCFG_PMUXEN;
+        PORT->Group[0].PMUX[24 / 2].reg = PORT_PMUX_PMUXE(6 /* CAN0 G */) | PORT_PMUX_PMUXO(6 /* CAN0 G */); /* have to write odd and even at once */
+
+        GCLK->PCHCTRL[CAN0_GCLK_ID].reg = GCLK_PCHCTRL_CHEN | GCLK_PCHCTRL_GEN_GCLK0;
+        MCLK->AHBMASK.reg |= MCLK_AHBMASK_CAN0;
+
+        NVIC_EnableIRQ(CAN0_IRQn);
+        break;
+    }
+    
+    if (mcan_configure_msg_ram(&mcan_cfg, &mcan_msg_ram_size)) {
+        Serial.println("RAM configuration succeeded");
+    } else {
+        return CAN_FAIL;
+    }
+    ret = mcan_initialize(&mcan, &mcan_cfg);
+    if (ret == 0) {
+        Serial.println("CAN initialized");
+    } else {
+        Serial.print("CAN init failed, code ");
+        Serial.println(ret);
+        return CAN_FAIL;
+    }
+    //mcan_set_tx_queue_mode(&mcan);
+    mcan_loopback_off(&mcan);
+    mcan_set_mode(&mcan, MCAN_MODE_CAN);
+    mcan_enable(&mcan);
+
+    
+    if (mcan_is_enabled(&mcan)) {
+        Serial.println("MCAN is enabled!");
+        return CAN_OK;
+    }
+
+    Serial.println("Something went wrong!!!!!!!!");
+    return CAN_FAIL;
+    
+    
 };
 uint8_t MCP_CAN::init_Mask(uint8_t num, uint8_t ext, uint32_t ulData)
 {
@@ -39,11 +131,38 @@ uint8_t MCP_CAN::init_Filt(uint8_t num, uint32_t ulData)
 }; // Initilize Filter(s)
 uint8_t MCP_CAN::setMode(uint8_t opMode)
 {
-    return 0;
+    return MCP_NORMAL;
 };                                        // Set operational mode
+
 uint8_t MCP_CAN::sendMsgBuf(uint32_t id, uint8_t ext, uint8_t len, uint8_t *buf)
 {
-    return 0;
+    uint8_t ret = 0xff;
+    if (!mcan_is_enabled(&mcan)) {
+        Serial.println("CAN not enabled!");
+        return CAN_FAILTX;
+    }
+    /*
+    txbuf = mcan_prepare_tx_buffer(&mcan, 0, id, len);
+    if (txbuf != NULL) {
+        memcpy(txbuf, buf, len);
+        mcan_send_tx_buffer(&mcan, 0);
+        ret = 0;
+    }
+    */
+    if (ext) {
+        id |= CAN_EXT_MSG_ID;
+    }
+    ret = mcan_enqueue_outgoing_msg(&mcan, id, len, buf);
+    Serial.print("Sending id: 0x");
+    Serial.print(id, HEX);
+    Serial.print("  len:");
+    Serial.print(len);
+    Serial.print("  ret:");
+    Serial.println(ret);
+    if (ret != 0xFF) {
+        return CAN_OK;
+    }
+    return CAN_FAILTX;
 };      // Send message to transmit buffer
 uint8_t MCP_CAN::sendMsgBuf(uint32_t id, uint8_t len, uint8_t *buf)
 {
@@ -51,11 +170,32 @@ uint8_t MCP_CAN::sendMsgBuf(uint32_t id, uint8_t len, uint8_t *buf)
 };                 // Send message to transmit buffer
 uint8_t MCP_CAN::readMsgBuf(uint32_t *id, uint8_t *ext, uint8_t *len, uint8_t *buf)
 {
-    return 0;
+    uint8_t msg_data[64];
+    struct mcan_msg_info msg;
+    msg.data = msg_data;
+    bool got_data;
+    if (mcan_is_tx_complete(&mcan)) {
+        mcan_clear_tx_flag(&mcan);
+        Serial.println("Clearing TX Complete Flag");
+    }
+    __disable_irq();
+    if ((got_data = rx_ded_buffer_data))
+        rx_ded_buffer_data = false;
+    __enable_irq();
+    
+    if (got_data) {
+        mcan_read_rx_buffer(&mcan, RX_BUFFER_0, &msg);
+        *id = mcan_get_id(msg.id);
+        *len = msg.data_len;
+        memcpy(buf, msg.data, *len);
+        return CAN_OK;
+    }
+    return CAN_NOMSG;
 };   // Read message from receive buffer
 uint8_t MCP_CAN::readMsgBuf(uint32_t *id, uint8_t *len, uint8_t *buf)
 {
-    return CAN_NOMSG;
+    uint8_t ext;
+    return readMsgBuf(id, &ext, len, buf);
 };               // Read message from receive buffer
 uint8_t MCP_CAN::checkReceive(void)
 {
@@ -85,3 +225,24 @@ uint8_t MCP_CAN::disOneShotTX(void)
 {
     return 0;
 };                                           // Disable one-shot transmission
+
+
+void CAN0_Handler(void)
+{
+    if (mcan_rx_array_data(&(use_object->mcan))) {
+        mcan_clear_rx_array_flag(&(use_object->mcan));
+        use_object->rx_ded_buffer_data = true;
+        Serial.println("Got a Packet 0!");
+    }
+}
+
+
+void CAN1_Handler(void)
+{
+    if (mcan_rx_array_data(&(use_object->mcan))) {
+        mcan_clear_rx_array_flag(&(use_object->mcan));
+        use_object->rx_ded_buffer_data = true;
+        Serial.println("Got a Packet 1!");
+    }
+}
+
--- src/samc21_can.h
+++ src/samc21_can.h
@@ -8,10 +8,56 @@
 #ifndef _SAMC_CAN_H_
 #define _SAMC_CAN_H_
 
+#include "sam.h"
+#include "mcan.h"
+#include "mcan_helper.h"
 #include "mcp_can_dfs.h"
 #define MAX_CHAR_IN_MESSAGE CAN_MAX_CHAR_IN_MESSAGE
 
 #include <inttypes.h>
+#include <string.h>
+
+/* size of our custom Rx and Tx Buffer Elements, in words */
+#define RAM_BUF_SIZE                  (MCAN_RAM_BUF_HDR_SIZE + 64u / 4)
+
+#define RAM_ARRAY_SIZE_FILT_STD       (8u)
+#define RAM_ARRAY_SIZE_FILT_EXT       (8u)
+#define RAM_FIFO_SIZE_RX0             (12u)
+/* no Rx FIFO 1 in our Message RAM */
+#define RAM_ARRAY_SIZE_RX             (4u)
+/* no Tx Event FIFO in our Message RAM */
+#define RAM_ARRAY_SIZE_TX             (0u)
+#define RAM_FIFO_SIZE_TX              (4u)
+
+
+/* size of our custom Message RAM, in words */
+#define MSG_RAM_SIZE      ( \
+      RAM_ARRAY_SIZE_FILT_STD * MCAN_RAM_FILT_STD_SIZE \
+    + RAM_ARRAY_SIZE_FILT_EXT * MCAN_RAM_FILT_EXT_SIZE \
+    + RAM_FIFO_SIZE_RX0 * RAM_BUF_SIZE \
+    + RAM_ARRAY_SIZE_RX * RAM_BUF_SIZE \
+    + RAM_ARRAY_SIZE_TX * RAM_BUF_SIZE \
+    + RAM_FIFO_SIZE_TX * RAM_BUF_SIZE )
+
+#define MSG_LEN_1_CAN     8
+#define MSG_LEN_1_CAN_FD  64
+#define MSG_LEN_2_CAN     7
+#define MSG_LEN_2_CAN_FD  48
+#define MSG_ID_2          (CAN_STD_MSG_ID | 0x444)
+#define MSG_ID_2_MASK     0x7FCul     /* bits 0 & 1 are don't care */
+#define RX_BUFFER_0       0
+#define RX_BUFFER_1       1
+#define FILTER_0          0
+#define FILTER_1          1
+
+struct frame_desc
+{
+    uint32_t id;
+    uint8_t data[64];
+    uint8_t len;
+    uint8_t buf_idx;
+};
+
 
 typedef struct {
     uint32_t id;
@@ -50,8 +96,16 @@ public:
     uint8_t errorCountTX(void);
     uint8_t enOneShotTX(void);
     uint8_t disOneShotTX(void);
-private:
 
+    struct mcan_set mcan;
+    volatile bool rx_ded_buffer_data;
+
+
+private:
+    uint32_t mcan_msg_ram[MSG_RAM_SIZE] __attribute__ ((aligned (4)));
+    uint32_t mcan_msg_ram_size = ARRAY_SIZE(mcan_msg_ram);
+    //uint8_t *txbuf;
+    
 };
 
 
